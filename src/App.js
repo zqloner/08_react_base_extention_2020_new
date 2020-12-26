523d03d5e386e760ad72440a0a67b192cf30dbbc
zhangqi <zqloner@163.com>
2020-12-26 12:58:15 +0800

    01_setState的两种写法

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
+import React,{Component} from "react";
+import SetStateDemo from "./01_setState";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SetStateDemo num={1}/>
+      </div>
+    );
+  }
 }
-
-export default App;
